f5f41d2034541530fac055403733391bc4fea366
Alex Sears <alexwsears@gmail.com>
2016-03-23 23:16:32 -0400

    Fixed port issue with heroku

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // Bring in our dependencies
 const app = require('express')();
 const routes = require('./routes');
+const PORT = process.env.PORT || 3000;
 
 //  Connect all our routes to our application
 app.use('/', routes);
 
 // Turn on that server!
-app.listen(3000, () => {
-  console.log('App listening on port 3000');
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
